87ab2d2b6218836ecc22e1c79b374e5406e256b7
jyksind1237 <64174767+jyksind1237@users.noreply.github.com>
2020-04-23 10:19:02 +0900

    Hello.java 생성

---
+++ Hello.java
@@ -0,0 +1,10 @@
+class WhileSum{
+  pulic static void main(String args[]) {
+    int i=1, sum=0;
+    while(i<100){
+      sum+sum+i;
+      i++;
+     }
+     Systeem.out.println(1에서 99까지 합은 " +sum);
+   }
+}
